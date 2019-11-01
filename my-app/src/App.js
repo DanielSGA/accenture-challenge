237e5aeadbfac8bd314e8c89e5a034d28ecbca59
Daniel Saldaña <danielsaldana@Daniels-MacBook-Pro.local>
2019-10-31 23:28:29 -0600

    Third commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import temp from './temp.jpg';
 import './styles/bootstrap.min.css'
-import { Nav } from './components/nav'
 import { Card } from './components/card'
 import './App.css';
 import {InputGroup, FormControl, Button, Row, Col, Image, Container} from 'react-bootstrap';
+import response from './server.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"> 
-
-        <div className="headtitle"> Plate Search 🚘</div>
-
+        <div className="headtitle">Plate Search 🚘</div>
         <Row>
           <Col md={{ span: 6, offset: 3 }}>
             <InputGroup className="mb-3 searchbar justify-content-md-center">
@@ -43,23 +41,7 @@ function App() {
               <Card img={temp}></Card>
             </Col>
           </Row>
-          
-          <Row>
-            <Col>
-              <Card img={temp}></Card>
-            </Col>
-            <Col>
-              <Card img={temp}></Card>
-            </Col>
-            <Col>
-              <Card img={temp}></Card>
-            </Col>
-            <Col className="right">
-              <Card img={temp}></Card>
-            </Col>
-          </Row>
         </Container>
-
       </header>
     </div>
     
--- my-app/src/server.js
+++ my-app/src/server.js
@@ -1,2 +1,23 @@
-const express = require('express')
-const router = express.Router()
+const aws = require('aws-sdk')
+const config = require('./config.json')
+
+(async function() {
+    try {
+        aws.config.setPromisesDependency();
+        aws.config.update({
+            accessKeyId: config.aws.accessKey,
+            secretAccessKey: config.aws.secretKey,
+        });
+
+        const s3 = new aws.S3();
+        const response = await s3.listObjectsV2({
+            Bucket: 'acn-bucket',
+            Prefix: 'plates'
+        }).promise();
+
+    } catch (e) {
+        console.log('Error', e)
+    }
+})();
+
+export default Response;
